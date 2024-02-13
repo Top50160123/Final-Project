c67ad80d34d505f80fdf9f4da0d8495165b612a3
top50160123 <thitikawja9@gmail.com>
2024-02-13 17:48:59 +0700

    fix redirect callback

--- src/auth/ProtectedRoute.jsx
+++ src/auth/ProtectedRoute.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import { Navigate } from 'react-router-dom'
 import { useUserAuth } from '../context/UserAuthContext'
+import { useUser } from './callback';
 
 function ProtectedRoute({ children }) {
 
     const { user } = useUserAuth();
+    const users = useUser();
 
     if (!user) {
         return <Navigate to="/" />
+    } else if(!users) {
+      return <Navigate to="/" />
     }
 
   return children;
