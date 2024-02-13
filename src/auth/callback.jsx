c031860bbc87ad96cc1090f25d9378dc42f1bc83
top50160123 <thitikawja9@gmail.com>
2024-02-13 18:00:46 +0700

    fix redirect callback

--- src/auth/callback.jsx
+++ src/auth/callback.jsx
@@ -1,5 +1,6 @@
-import { useState, useEffect } from 'react';  import qs from 'qs';
-import { useNavigate, useLocation } from 'react-router-dom';
+import { useState, useEffect } from "react";
+import qs from "qs";
+import { useNavigate, useLocation } from "react-router-dom";
 
 const CallbackPage = () => {
   const location = useLocation();
@@ -14,19 +15,22 @@ const CallbackPage = () => {
 
         if (codeFromURL) {
           try {
-            const response = await fetch(`https://server-node-tau.vercel.app/getToken/${codeFromURL}`, {
-              method: "POST",
-              headers: {
-                "Content-Type": "application/json",
-              },
-            });
-      
+            const response = await fetch(
+              `https://server-node-tau.vercel.app/getToken/${codeFromURL}`,
+              {
+                method: "POST",
+                headers: {
+                  "Content-Type": "application/json",
+                },
+              }
+            );
+
             if (!response.ok) {
               throw new Error(`HTTP error! Status: ${response.status}`);
             }
-      
+
             const result = await response.json();
-            setAccessToken(result.access_token);              
+            setAccessToken(result.access_token);
             console.log("result :", result.access_token);
           } catch (error) {
             console.error("Wrong to Sign", error.message || "Wrong to Sign");
@@ -42,29 +46,34 @@ const CallbackPage = () => {
     const fetchUserData = async () => {
       if (accessToken) {
         try {
-          const response = await fetch('https://misapi.cmu.ac.th/cmuitaccount/v1/api/cmuitaccount/basicinfo', {
-            headers: {
-              'Authorization': `Bearer ${accessToken}`
+          const response = await fetch(
+            "https://misapi.cmu.ac.th/cmuitaccount/v1/api/cmuitaccount/basicinfo",
+            {
+              method: "GET",
+              headers: {
+                Authorization: `Bearer ${accessToken}`,
+              },
             }
-          });
+          );
           const userData = await response.json();
-          setUserData(userData);
-          if(userData){
-            navigate("/");
-            console.log("Navigate Done");
-          }
+          console.log("userData", userData);
+          // setUserData(userData);
+          // if(userData){
+          //   navigate("/");
+          //   console.log("Navigate Done");
+          // }
         } catch (error) {
-          console.error('Error fetching user data:', error.message);
+          console.error("Error fetching user data:", error.message);
         }
       }
     };
 
     fetchUserData();
   }, [accessToken, navigate]);
-  
-  console.log("userData:",userData)
+
+  // console.log("userData:",userData)
 
   return null;
-}
+};
 
 export default CallbackPage;
